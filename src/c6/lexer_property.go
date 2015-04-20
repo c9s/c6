8d75ae129d161382d04eac2a27f3fe82c26ad4da
c9s <yoanlin93@gmail.com>
2015-04-20 20:12:16 +0800

    Move lexProperty methods to new file

---
+++ src/c6/lexer_property.go
@@ -0,0 +1,59 @@
+package c6
+
+import "unicode"
+
+// import "strings"
+import "fmt"
+import "errors"
+
+func lexPropertyName(l *Lexer) stateFn {
+	var r rune = l.next()
+	for r == '-' || unicode.IsLetter(r) {
+		r = l.next()
+	}
+	l.backup()
+	l.emit(T_PROPERTY_NAME)
+	lexColon(l)
+	return lexPropertyValue
+}
+
+func lexColon(l *Lexer) stateFn {
+	l.ignoreSpaces()
+	var r = l.next()
+	if r == ':' {
+		l.emit(T_COLON)
+	} else {
+		l.error("Expecting ':' token, Got '%s'", r)
+	}
+	return nil
+}
+
+func lexPropertyValue(l *Lexer) stateFn {
+	l.ignoreSpaces()
+	var r rune = l.peek()
+
+	if r == '#' && l.peekMore(2) == '{' {
+		return lexInterpolation
+	} else if r == '#' {
+		return lexHexColor
+	} else if unicode.IsDigit(r) {
+		return lexNumber
+	} else if unicode.IsLetter(r) {
+		return lexConstantString
+	} else if r == '$' {
+		return lexVariable
+	} else if r == ' ' {
+		l.next()
+		l.ignore()
+		return lexPropertyValue
+	} else if r == ';' {
+		l.next()
+		l.emit(T_SEMICOLON)
+		return lexStatement
+	} else if r == EOF {
+		l.error("Unexpected end of file", r)
+	} else {
+		l.error("can't lex rune for property value: '%s'", r)
+	}
+	return nil
+}
