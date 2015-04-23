773b00a12e1fecf5b193c87dd2e39150751cb424
c9s <yoanlin93@gmail.com>
2015-04-24 00:13:52 +0800

    Add ruleset test

---
+++ src/c6/ast/ruleset_test.go
@@ -0,0 +1,11 @@
+package ast
+
+import "github.com/stretchr/testify/assert"
+import "testing"
+
+func TestRuleSet(t *testing.T) {
+	sel := IdSelector{"myId"}
+	combinedSel := CombinedSelector{"", []Selector{sel}}
+	ruleset := RuleSet{&combinedSel, DeclarationBlockl{}}
+	_ = ruleset
+}
