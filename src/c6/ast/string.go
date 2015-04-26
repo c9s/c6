f5e7158227c5ab006612637b66ff346a3e66eed9
c9s <yoanlin93@gmail.com>
2015-04-26 20:11:45 +0800

    Add String struct for "..." and '...'

---
+++ src/c6/ast/string.go
@@ -0,0 +1,12 @@
+package ast
+
+type String struct {
+	Value string
+	Token *Token
+}
+
+func (self String) CanBeExpression() {}
+
+func NewString(token *Token) *String {
+	return &String{token.Str, token}
+}
