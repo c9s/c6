f37c965aa11aa832f181310ea6fcd8e848d2809d
c9s <yoanlin93@gmail.com>
2015-04-27 11:37:07 +0800

    Add Color interface

--- src/c6/ast/color.go
+++ src/c6/ast/color.go
@@ -1,11 +1,17 @@
 package ast
 
+type Color interface {
+	CanBeColor()
+}
+
 type HexColor struct {
 	Hex   string
 	Token *Token
 }
 
 func (self HexColor) CanBeExpression() {}
+func (self HexColor) CanBeNode()       {}
+func (self HexColor) CanBeColor()      {}
 
 // Factor functions
 func NewHexColor(hex string, token *Token) *HexColor {
@@ -21,7 +27,24 @@ type RGBAColor struct {
 }
 
 func (self RGBAColor) CanBeExpression() {}
+func (self RGBAColor) CanBeNode()       {}
+func (self RGBAColor) CanBeColor()      {}
 
 func NewRGBAColor(r, g, b, a float64, token *Token) *RGBAColor {
 	return &RGBAColor{r, g, b, a, token}
+}
+
+type RGBColor struct {
+	R     float64
+	G     float64
+	B     float64
+	Token *Token
+}
+
+func (self RGBColor) CanBeExpression() {}
+func (self RGBColor) CanBeNode()       {}
+func (self RGBColor) CanBeColor()      {}
+
+func NewRGBColor(r, g, b float64, token *Token) *RGBColor {
+	return &RGBColor{r, g, b, token}
 }
