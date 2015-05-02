2af5d7081dae22404521ecdf00b3bdf59214b500
c9s <yoanlin93@gmail.com>
2015-05-02 16:59:18 +0800

    Add more number operation

--- src/c6/ast/number.go
+++ src/c6/ast/number.go
@@ -44,22 +44,98 @@ func (num Number) String() (out string) {
 	return out
 }
 
+/*
+For color type, we treat them as vector. a vector can be computed with scalar or another vector.
+
+
+Valid expression:
+
+	#aaa / 3
+	#bb * 3
+	#bb - #cc
+	3px - 3px
+	3px + 3px
+
+Invalid expression
+
+- Number can't be the dividend.
+- Hex Color can't divisor.
+
+	3 / #aaa
+	3 - #bbb
+	3px - 3
+	6em - 3px
+
+*/
+
 // Check the unit of the number operands to see if they're computable.
-func NumberComputable(a *Number, b *Number) bool {
+func NumberUnitCompatible(a *Number, b *Number) bool {
 	var unitA = a.GetUnit()
 	var unitB = b.GetUnit()
 	// If any of them is without unit
 	return unitA == UNIT_NONE || unitB == UNIT_NONE || unitA == unitB
 }
 
-// Pass numbers as pointer
-func NumberAdd(a *Number, b *Number) *Number {
-	if !NumberComputable(a, b) {
+func NumberSub(a *Number, b *Number) *Number {
+	var unitA = a.GetUnit()
+	var unitB = b.GetUnit()
+	if unitA != unitB {
 		panic("Can't compute number: incompatible number unit.")
 	}
 
+	var result = a.Value - b.Value
+	var num = NewNumber(result, nil)
+	num.SetUnit(unitA)
+	return num
+}
+
+func NumberDiv(a *Number, b *Number) *Number {
 	var unitA = a.GetUnit()
 	var unitB = b.GetUnit()
+	if !(unitA == UNIT_NONE || unitB == UNIT_NONE || unitA == unitB) {
+		panic("Invalid number multiply expression")
+	}
+	var unitC UnitType = UNIT_NONE
+	if unitA != UNIT_NONE {
+		unitC = unitA
+	} else if unitB != UNIT_NONE {
+		unitC = unitB
+	}
+
+	var result = a.Value * b.Value
+	var num = NewNumber(result, nil)
+	num.SetUnit(unitC)
+	return num
+	return nil
+}
+
+func NumberMul(a *Number, b *Number) *Number {
+	var unitA = a.GetUnit()
+	var unitB = b.GetUnit()
+	if !(unitA == UNIT_NONE || unitB == UNIT_NONE || unitA == unitB) {
+		panic("Invalid number multiply expression")
+	}
+
+	var unitC UnitType = UNIT_NONE
+	if unitA != UNIT_NONE {
+		unitC = unitA
+	} else if unitB != UNIT_NONE {
+		unitC = unitB
+	}
+
+	var result = a.Value * b.Value
+	var num = NewNumber(result, nil)
+	num.SetUnit(unitC)
+	return num
+}
+
+func NumberAdd(a *Number, b *Number) *Number {
+	var unitA = a.GetUnit()
+	var unitB = b.GetUnit()
+
+	if unitA != unitB {
+		panic("Can't compute number: incompatible number unit.")
+	}
 
 	var unitC UnitType = UNIT_NONE
 	if unitA != UNIT_NONE {
