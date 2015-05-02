6950d60c5987130fb9c51838b7b876e932193170
c9s <yoanlin93@gmail.com>
2015-05-03 00:43:45 +0800

    Add RGBToHSV method

--- src/c6/ast/color.go
+++ src/c6/ast/color.go
@@ -2,6 +2,7 @@ package ast
 
 import "strconv"
 import "fmt"
+import "math"
 
 type Hex string
 
@@ -117,4 +118,39 @@ func (self RGBColor) String() string {
 
 func NewRGBColor(r, g, b uint8, token *Token) *RGBColor {
 	return &RGBColor{r, g, b, token}
+}
+
+func RGBToHSV(ir, ig, ib uint) (h, s, v float64) {
+	// cast to float64 for math.* API
+	var r = float64(ir)
+	var g = float64(ig)
+	var b = float64(ib)
+
+	var min = math.Min(math.Min(r, g), b)
+
+	v = math.Max(math.Max(r, g), b)
+	var C = v - min
+
+	s = 0.0
+	if v != 0.0 {
+		s = C / v
+	}
+
+	h = 0.0 // We use 0 instead of undefined as in wp.
+	if min != v {
+		if v == r {
+			h = math.Mod((g-b)/C, 6.0)
+		}
+		if v == g {
+			h = (b-r)/C + 2.0
+		}
+		if v == b {
+			h = (r-g)/C + 4.0
+		}
+		h *= 60.0
+		if h < 0.0 {
+			h += 360.0
+		}
+	}
+	return
 }
