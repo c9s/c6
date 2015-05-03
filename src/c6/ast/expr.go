32daa57e25c23de07a302c03cb9cbad97d6c7329
c9s <yoanlin93@gmail.com>
2015-05-03 16:27:37 +0800

    Pass SymTable object to Evaluate method

--- src/c6/ast/expr.go
+++ src/c6/ast/expr.go
@@ -14,15 +14,24 @@ func NewUnaryExpression(op OpType, expr Expression) *UnaryExpression {
 	return &UnaryExpression{op, expr}
 }
 
-func (self *UnaryExpression) Evaluate() Value {
-	if self.Op == OpAdd {
-		// TODO: call Evaluate()
-		return Value(self.Expr)
-	} else if self.Op == OpSub {
-		// TODO: call Evaluate()
-		return Value(self.Expr)
+func (self *UnaryExpression) Evaluate(symTable *SymTable) Value {
+	var val Value = nil
+	if expr, ok := self.Expr.(*BinaryExpression); ok {
+		val = expr.Evaluate(symTable)
+	} else if expr, ok := self.Expr.(*UnaryExpression); ok {
+		val = expr.Evaluate(symTable)
 	}
-	return nil
+
+	// negative value
+	if self.Op == OpSub {
+		switch n := val.(type) {
+		case *Number:
+			n.Value = -n.Value
+		case *Length:
+			n.Value = -n.Value
+		}
+	}
+	return val
 }
 
 func (self UnaryExpression) String() string {
@@ -44,17 +53,43 @@ func (self BinaryExpression) String() (out string) {
 	return out
 }
 
-func (self *BinaryExpression) Evaluate() Value {
-	if self.Op == OpAdd {
-		// TODO: call Evaluate()
-		return Value(self.Left)
-	} else if self.Op == OpSub {
-		// TODO: call Evaluate()
-		return Value(self.Left)
+func (self *BinaryExpression) Evaluate(symTable *SymTable) Value {
+	var lval Value = nil
+	var rval Value = nil
+
+	switch expr := self.Left.(type) {
+	case *BinaryExpression:
+		lval = expr.Evaluate(symTable)
+	case *UnaryExpression:
+		lval = expr.Evaluate(symTable)
+	}
+	switch expr := self.Right.(type) {
+	case *UnaryExpression:
+		rval = expr.Evaluate(symTable)
+	case *BinaryExpression:
+		rval = expr.Evaluate(symTable)
+	}
+	if lval != nil && rval != nil {
+		return Compute(self.Op, lval, rval)
 	}
 	return nil
 }
 
 func NewBinaryExpression(op OpType, left Expression, right Expression) *BinaryExpression {
 	return &BinaryExpression{op, left, right}
+}
+
+func Compute(op OpType, a Value, b Value) Value {
+
+	switch op {
+	case OpAdd:
+		switch ta := a.(type) {
+		case *Length:
+			switch tb := b.(type) {
+			case *Length:
+				return LengthAddLength(ta, tb)
+			}
+		}
+	}
+	return nil
 }
