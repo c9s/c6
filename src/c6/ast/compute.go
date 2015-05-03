65e1b367a93f3eb36996521c88fb4b1fdf01fb11
c9s <yoanlin93@gmail.com>
2015-05-03 20:33:09 +0800

    Move Compute function to compute.go

--- src/c6/ast/compute.go
+++ src/c6/ast/compute.go
@@ -60,3 +60,97 @@ var computeFunctionMatrix [5][5]ComputeFunction = [5][5]ComputeFunction{
 	/* RGBColorValue */
 	[5]ComputeFunction{nil, nil, nil, nil, nil},
 }
+
+func Compute(op OpType, a Value, b Value) Value {
+	switch op {
+	case OpAdd:
+		switch ta := a.(type) {
+		case *Number:
+			switch tb := b.(type) {
+			case *Number:
+				return NumberAddNumber(ta, tb)
+			case *HexColor:
+				return HexColorAddNumber(tb, ta)
+			}
+		case *Length:
+			switch tb := b.(type) {
+			case *Length:
+				return LengthAddLength(ta, tb)
+			}
+		case *HexColor:
+			switch tb := b.(type) {
+			case *Number:
+				return HexColorAddNumber(ta, tb)
+			}
+		case *RGBColor:
+			switch tb := b.(type) {
+			case *Number:
+				return RGBColorAddNumber(ta, tb)
+			}
+		case *RGBAColor:
+			switch tb := b.(type) {
+			case *Number:
+				return RGBAColorAddNumber(ta, tb)
+			}
+		}
+	case OpSub:
+		switch ta := a.(type) {
+
+		case *Number:
+			switch tb := b.(type) {
+			case *Number:
+				return NumberSubNumber(ta, tb)
+			}
+
+		case *Length:
+			switch tb := b.(type) {
+			case *Length:
+				val := LengthSubLength(ta, tb)
+				fmt.Printf("Substracted value: %+v\n", val)
+				return val
+			}
+
+		case *HexColor:
+			switch tb := b.(type) {
+			case *Number:
+				return HexColorSubNumber(ta, tb)
+			}
+
+		case *RGBColor:
+			switch tb := b.(type) {
+			case *Number:
+				return RGBColorSubNumber(ta, tb)
+			}
+
+		case *RGBAColor:
+			switch tb := b.(type) {
+			case *Number:
+				return RGBAColorSubNumber(ta, tb)
+			}
+		}
+	case OpMul:
+		switch ta := a.(type) {
+
+		case *Length:
+			switch tb := b.(type) {
+			case *Length:
+				return LengthMulLength(ta, tb)
+			case *Number:
+				return LengthMulNumber(ta, tb)
+			}
+
+		case *RGBColor:
+			switch tb := b.(type) {
+			case *Number:
+				return RGBColorMulNumber(ta, tb)
+			}
+
+		case *RGBAColor:
+			switch tb := b.(type) {
+			case *Number:
+				return RGBAColorMulNumber(ta, tb)
+			}
+		}
+	}
+	return nil
+}
