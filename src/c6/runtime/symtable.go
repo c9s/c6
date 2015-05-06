9f23c836f2de82f36622a5a83edf4d3e1fe0890a
c9s <yoanlin93@gmail.com>
2015-05-06 19:55:21 +0800

    Improve SymTable struct & methods

--- src/c6/runtime/symtable.go
+++ src/c6/runtime/symtable.go
@@ -1,22 +1,24 @@
 package runtime
 
-import "c6/ast"
+// import "c6/ast"
 
-type SymTable map[string]*ast.Variable
+type SymTableItem interface{}
 
-func (self SymTable) AddVariable(v *ast.Variable) {
-	self[v.Name] = v
+type SymTable map[string]SymTableItem
+
+func (self SymTable) Set(name string, v SymTableItem) {
+	self[name] = v
 }
 
-func (self SymTable) FindVariable(name string) *ast.Variable {
+func (self SymTable) Get(name string) SymTableItem {
 	if val, ok := self[name]; ok {
 		return val
 	}
 	return nil
 }
 
-func (self SymTable) HasVariable(v *ast.Variable) bool {
-	if _, ok := self[v.Name]; ok {
+func (self SymTable) Has(name string) bool {
+	if _, ok := self[name]; ok {
 		return true
 	} else {
 		return false
