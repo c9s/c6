be78c9b623d8f31d516bdb4f89d1f0b14865dc6d
c9s <yoanlin93@gmail.com>
2015-05-06 23:09:29 +0800

    Add AppendElseIf method to IfStatement

--- src/c6/ast/if.go
+++ src/c6/ast/if.go
@@ -3,10 +3,14 @@ package ast
 type If struct {
 	Condition Expression
 	Block     *Block
-	ElseIf    []*If
+	ElseIfs   []*If
 }
 
 func (stm If) CanBeStatement() {}
+
+func (stm If) AppendElseIf(ifStm *If) {
+	stm.ElseIfs = append(stm.ElseIfs, ifStm)
+}
 
 func (stm If) String() string {
 	return "(if statement STRING() un-implemented)"
