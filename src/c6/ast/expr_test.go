3451a82782d92da1c8ce54ecb3b9e1bdbaa7806c
c9s <yoanlin93@gmail.com>
2015-05-08 01:47:21 +0800

    Use assertion

--- src/c6/ast/expr_test.go
+++ src/c6/ast/expr_test.go
@@ -1,12 +1,11 @@
 package ast
 
 import "testing"
-
-// import "github.com/stretchr/testify/assert"
+import "github.com/stretchr/testify/assert"
 
 func TestBinaryExprString(t *testing.T) {
 	var num1 = NewNumber(2, NewUnit(T_UNIT_PX, nil), nil)
 	var num2 = NewNumber(3, NewUnit(T_UNIT_PX, nil), nil)
 	var expr = NewBinaryExpression(NewOp(T_DIV, nil), num1, num2, false)
-	t.Logf("%s", expr.String())
+	assert.Equal(t, "2px/3px", expr.String())
 }
