4c2481163dff1f1076de9d724b40cce8bc2452da
c9s <yoanlin93@gmail.com>
2015-05-11 13:57:24 +0800

    Add @media only screen

--- src/c6/parser_test.go
+++ src/c6/parser_test.go
@@ -150,6 +150,11 @@ func TestParserMediaQueryNotScreen(t *testing.T) {
 	assert.Equal(t, 1, len(stmts))
 }
 
+func TestParserMediaQueryOnlyScreen(t *testing.T) {
+	var stmts = RunParserTest(`@media only screen { .red { color: red; } }`)
+	assert.Equal(t, 1, len(stmts))
+}
+
 func TestParserMediaQueryAllAndMinWidth(t *testing.T) {
 	var stmts = RunParserTest(`@media all and (min-width:500px) {  .red { color: red; } }`)
 	assert.Equal(t, 1, len(stmts))
