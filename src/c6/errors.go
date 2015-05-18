3ae9cc1a470b33d453ca773b858722feb5d11a3b
c9s <yoanlin93@gmail.com>
2015-05-18 15:24:32 +0800

    move error structs to errors.go

---
+++ src/c6/errors.go
@@ -0,0 +1,51 @@
+package c6
+
+import "fmt"
+
+/*
+User's fault, probably.
+
+Struct for common syntax error.
+
+Examples:
+
+
+panic(SyntaxError{
+	Expecting: ...
+})
+*/
+type SyntaxError struct {
+	Expecting   string
+	ActualToken *ast.Token
+	Guide       string
+	GuideUrl    string
+}
+
+func (err SyntaxError) Error() (out string) {
+	out = "Syntax error "
+	if err.ActualToken != nil {
+		out += fmt.Sprintf(" on line %d, offset %d. given %s\n", err.ActualToken.Line, err.ActualToken.Pos, err.ActualToken.Type.String())
+	}
+	if err.Expecting != "" {
+		out += "The parser expects " + err.Expecting + "\n"
+	}
+	if err.Guide != "" {
+		out += "We suggest you to " + err.Guide + "\n"
+	}
+	if err.GuideUrl != "" {
+		out += "For more information, please visit " + err.GuideUrl + "\n"
+	}
+	return out
+}
+
+/*
+Parser's fault
+*/
+type ParserError struct {
+	ExpectingToken string
+	ActualToken    string
+}
+
+func (e ParserError) Error() string {
+	return fmt.Sprintf("Expecting '%s', but the actual token we got was '%s'.", e.ExpectingToken, e.ActualToken)
+}
