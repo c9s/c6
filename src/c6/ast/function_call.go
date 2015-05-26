5cefa2d536270b1bc1bbb15cdabcd956947dbb50
c9s <yoanlin93@gmail.com>
2015-05-26 15:11:48 +0800

    Move function call arguments to a single file.

--- src/c6/ast/function_call.go
+++ src/c6/ast/function_call.go
@@ -1,52 +1,5 @@
 package ast
 
-import "sort"
-
-// type FunctionCallArgument Expression
-type FunctionCallArgument struct {
-	Argument                Expression
-	ArgumentDefineReference *Argument
-}
-
-func (arg FunctionCallArgument) String() (out string) {
-	out = arg.Argument.String()
-	if arg.ArgumentDefineReference != nil {
-		out += " ref:" + arg.ArgumentDefineReference.String()
-	}
-	return out
-}
-
-func NewFunctionCallArgument(expr Expression) *FunctionCallArgument {
-	return &FunctionCallArgument{expr, nil}
-}
-
-type FunctionCallArguments []*FunctionCallArgument
-
-func (args *FunctionCallArguments) Sort() {
-	var sorter = &FunctionCallArgumentsSorter{*args}
-	sort.Sort(sorter)
-}
-
-type FunctionCallArgumentsSorter struct {
-	Arguments FunctionCallArguments
-}
-
-// Len is part of sort.Interface.
-func (s *FunctionCallArgumentsSorter) Len() int {
-	return len(s.Arguments)
-}
-
-// Swap is part of sort.Interface.
-func (s *FunctionCallArgumentsSorter) Swap(i, j int) {
-	s.Arguments[i], s.Arguments[j] = s.Arguments[j], s.Arguments[i]
-}
-
-// Less is part of sort.Interface. It is implemented by calling the "by" closure in the sorter.
-func (s *FunctionCallArgumentsSorter) Less(i, j int) bool {
-	return s.Arguments[i].ArgumentDefineReference.Position < s.Arguments[j].ArgumentDefineReference.Position
-	// return s.by(&s.Arguments[i], &s.Arguments[j])
-}
-
 type FunctionCall struct {
 	Ident     *Token
 	Arguments FunctionCallArguments
