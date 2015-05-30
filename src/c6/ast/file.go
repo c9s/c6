7ed906f2749175a7182e15484b154f4026251f97
c9s <yoanlin93@gmail.com>
2015-05-30 21:04:33 +0800

    Add ast.File struct for saving file information

---
+++ src/c6/ast/file.go
@@ -0,0 +1,23 @@
+package ast
+
+import "os"
+import "io/ioutil"
+
+type File struct {
+	Scope    *Scope
+	FileName string
+	FileInfo os.FileInfo
+}
+
+// NewFile stat the file to get the file info
+func NewFile(filename string) (*File, error) {
+	fi, err := os.Stat(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &File{FileName: filename, FileInfo: fi}, nil
+}
+
+func (f *File) ReadFile() ([]byte, error) {
+	return ioutil.ReadFile(f.FileName)
+}
