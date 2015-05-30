3d87ad49a29e8b8092dde336a3434b7e86955720
c9s <yoanlin93@gmail.com>
2015-05-30 20:05:12 +0800

    Add position object

---
+++ src/c6/ast/position.go
@@ -0,0 +1,26 @@
+package ast
+
+type PositionProvider interface {
+	Position() Position
+}
+
+type Position struct {
+	Filename string // filename, if any
+	Offset   int    // offset, starting at 0
+	Line     int    // line number, starting at 1
+	Column   int    // column number, starting at 1 (byte count)
+}
+
+func (pos Position) String() string {
+	s := pos.Filename
+	if pos.IsValid() {
+		if s != "" {
+			s += ":"
+		}
+		s += fmt.Sprintf("%d:%d", pos.Line, pos.Column)
+	}
+	if s == "" {
+		s = "-"
+	}
+	return s
+}
