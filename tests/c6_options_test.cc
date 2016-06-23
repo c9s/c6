4850768a219678bbef0ea4ded9a5b1042795f334
c9s <yoanlin93@gmail.com>
2016-06-23 15:08:08 +0800

    Fix cmake test

--- tests/c6_options_test.cc
+++ tests/c6_options_test.cc
@@ -1,16 +1,13 @@
 #include <stdlib.h>
 #include "gtest/gtest.h"
 
-TEST(CompositionOptionTest, Constructor) {
-    // CompositeOptions o(52, 68, 10, RightBottom);
-    // o.set_thumb_resize_width(500);
-}
-
 TEST(CompositionOptionTest, AddColorsFromImage) {
-
+  EXPECT_EQ(1, 1);
 }
 
+/*
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+*/
